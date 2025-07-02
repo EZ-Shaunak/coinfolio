a838a5095aa5184557b4c161bb65ebe259285e7e
EZ-Shaunak <shaunak4kulkarni@gmail.com>
2025-07-02 11:18:56 -0400

    basic structure of coinfolio, added next, react scaffolding

---
+++ coinfolio/app/layout.js
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Josefin_Sans } from 'next/font/google'
+
+
+const josefin_sans = Josefin_Sans({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'CoinFolio',
+  description: 'Your personal crypto portfolio',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={josefin_sans.className}>
+
+
+        {children}
+
+      </body>
+    </html>
+  )
+}
